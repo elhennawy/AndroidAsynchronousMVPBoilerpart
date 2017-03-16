e8639adc569c6e3b945d5e07387b72f8d6866ec1
Ovidiu CONEAC <ovidiu.coneac@gmail.com>
2017-03-16 17:19:57 +0200

    Implemente unit tests for view

--- app/src/main/java/ro/ovidiuconeac/androidasynchronousmvpboilerpart/features/featurex/view/XView.java
+++ app/src/main/java/ro/ovidiuconeac/androidasynchronousmvpboilerpart/features/featurex/view/XView.java
@@ -6,7 +6,7 @@ import ro.ovidiuconeac.androidasynchronousmvpboilerpart.features.Screen;
  * Created by ovidiu on 12/29/16.
  */
 
-public interface XView  extends Screen {
+public interface XView extends Screen {
     void requestAction1();
     void postResult1(String result);
     void requestAction2();
---
+++ app/src/test/java/ro/ovidiuconeac/androidasynchronousmvpboilerpart/features/featurex/view/XViewTest.java
@@ -0,0 +1,65 @@
+package ro.ovidiuconeac.androidasynchronousmvpboilerpart.features.featurex.view;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ro.ovidiuconeac.androidasynchronousmvpboilerpart.features.Presenter;
+import ro.ovidiuconeac.androidasynchronousmvpboilerpart.features.Screen;
+import ro.ovidiuconeac.androidasynchronousmvpboilerpart.features.featurex.presenter.XPresenterImpl;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by ovidiu on 1/2/17.
+ */
+public class XViewTest {
+
+    private XActivity view;
+    private Screen screen;
+
+    @Before
+    public void setUp() {
+        view = mock(XActivity.class);
+        screen = mock(Screen.class);
+    }
+
+    @After
+    public void tearDown() {
+        view = null;
+    }
+
+    @Test
+    public void testRequestAction1() {
+        doNothing().when(view).requestAction1();
+        view.requestAction1();
+    }
+
+    @Test
+    public void testRequestAction2() {
+        doNothing().when(view).requestAction2();
+        view.requestAction2();
+    }
+
+    @Test
+    public void testRequestAction3() {
+        doNothing().when(view).requestAction3();
+        view.requestAction3();
+    }
+
+    @Test
+    public void testCachePresenter() {
+        doNothing().when(view).cachePresenter(any(Presenter.class));
+        view.cachePresenter(any(Presenter.class));
+    }
+
+    @Test
+    public void testRestorePresenter() {
+        doNothing().when(view).restorePresenter(any(UUID.class));
+        view.restorePresenter(any(UUID.class));
+    }
+}
